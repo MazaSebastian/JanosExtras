d3aa8f2e5daba181581b9d9dae9f67571d2fb468
MazaSebastian <mazasebastian@hotmail.com>
2025-11-25 21:18:13 -0300

    Fix: Agregar import de getAuth en CoordinacionesPanel

--- frontend/src/components/CoordinacionesPanel.js
+++ frontend/src/components/CoordinacionesPanel.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
 import { coordinacionesAPI, salonesAPI, authAPI } from '@/services/api';
+import { getAuth } from '@/utils/auth';
 import { SkeletonCard } from '@/components/Loading';
 import styles from '@/styles/CoordinacionesPanel.module.css';
 import { FLUJOS_POR_TIPO } from '@/components/CoordinacionFlujo';
